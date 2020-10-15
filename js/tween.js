8e968df91c822b0cd5a2f0a34bb19aabb3007af2
Raggi Izar <61994369+Raggiiz@users.noreply.github.com>
2020-10-15 14:42:15 -0300

    Add files via upload

---
+++ js/tween.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    $('#about').mousemove(function(event) {
+        cx = Math.ceil($('body').width() * 0.5);
+        cy = Math.ceil($('body').height() * 0.5);
+        dx = event.pageX - cx;
+        dy = event.pageY - cy;
+
+        tiltx = (dy / cy);
+        tilty = - (dx / cx);
+        radius = Math.sqrt(Math.pow(tiltx,2) + Math.pow(tilty,2));
+        degree = (radius * 8);
+        TweenLite.set("#container", {transform:'rotate3d(' + tiltx + ', ' + tilty + ', 0, ' + degree + 'deg)'});
+    });
+    
+});
